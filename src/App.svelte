33aeb1d61e498f764d164bf17ed3325c157e07d3
snl95 <snl95@txstate.edu>
2022-04-28 15:41:17 -0500

    add new poll form and basic formatting

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,7 @@
 	import Footer from './components/Footer.svelte';
 	import Tabs from './shared/Tabs.svelte';
 	import CreatePollForm from './components/CreatePollForm.svelte';
-
+	import PollList from './components/PollList.svelte';
 	// tabs
 	let items = ['Current Polls', 'Add New Poll'];
 	let activeItem = 'Current Polls';
@@ -11,15 +11,31 @@
 		activeItem = e.detail;
 		console.log(activeItem);
 	};
+
+	let polls = [
+		{
+			id: 1,
+			title: 'Poll 1',
+			question: 'Python or JavaScript?',
+			votesA: 9,
+			votesB: 2,
+		},
+	];
+
+	const handleAddPoll = (e) => {
+		const poll = e.detail;
+		polls = [...polls, poll];
+		activeItem = 'Current Polls';
+	};
 </script>
 
 <Logo />
 <main>
 	<Tabs {items} {activeItem} on:tabChange={tabChange} />
 	{#if activeItem === 'Current Polls'}
-		<p>Current Polls</p>
+		<PollList {polls} />
 	{:else if activeItem === 'Add New Poll'}
-		<CreatePollForm />
+		<CreatePollForm on:addPoll={handleAddPoll} />
 	{/if}
 </main>
 <Footer />
--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,25 +1,60 @@
 <script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	import Button from '../shared/Button.svelte';
 	let fields = { question: '', answerA: '', answerB: '' };
+	let errors = { question: '', answerA: '', answerB: '' };
+	let valid = false;
 
 	const handleSubmit = () => {
-		console.log(fields);
+		valid = true;
+		// validate question
+		if (fields.question.trim().length < 5) {
+			valid = false;
+			errors.question = 'Question must be at least 5 characters';
+		} else {
+			errors.question = '';
+		}
+		// validate answerA
+		if (fields.answerA.trim().length < 0) {
+			valid = false;
+			errors.answerA = 'Answer A must be at least 1 character';
+		} else {
+			errors.answerA = '';
+		}
+		// validate answerB
+		if (fields.answerB.trim().length < 0) {
+			valid = false;
+			errors.answerB = 'Answer B must be at least 1 character';
+		} else {
+			errors.answerB = '';
+		}
+		// if valid, submit form
+		if (valid) {
+			let poll = { ...fields, votesA: 0, votesB: 0, id: Math.random() };
+			dispatch('addPoll', poll);
+		}
 	};
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
 	<div class="form-field">
 		<label for="question">Poll Question:</label>
-		<input type="text" name="question" id="question" placeholder="Question" bind:value={fields.question} />
+		<input required type="text" name="question" id="question" placeholder="Question" bind:value={fields.question} />
+		<div class="errors">{errors.question}</div>
 	</div>
 	<div class="form-field">
 		<label for="answer-a">Answer A</label>
-		<input type="text" name="answer-a" id="answer-a" placeholder="Answer A" bind:value={fields.answerA} />
+		<input required type="text" name="answer-a" id="answer-a" placeholder="Answer A" bind:value={fields.answerA} />
+		<div class="errors">{errors.answerA}</div>
 	</div>
 	<div class="form-field">
 		<label for="answer-b">Answer B</label>
-		<input type="text" name="answer-b" id="answer-b" placeholder="Answer B" bind:value={fields.answerB} />
+		<input required type="text" name="answer-b" id="answer-b" placeholder="Answer B" bind:value={fields.answerB} />
+		<div class="errors">{errors.answerB}</div>
 	</div>
-	<button>Add Poll</button>
+	<Button type="secondary" flat={true}>Create Poll</Button>
 </form>
 
 <style>
@@ -38,5 +73,10 @@
 	input {
 		width: 100%;
 		border-radius: 6px;
+	}
+	.errors {
+		color: #d91b42;
+		font-weight: bold;
+		font-size: 12px;
 	}
 </style>
