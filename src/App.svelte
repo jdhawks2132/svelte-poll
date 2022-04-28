17beb1dcc3a440bbdbe4626719e857ab7da1c71f
snl95 <snl95@txstate.edu>
2022-04-28 14:51:35 -0500

    add tab change functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,20 @@
 <script>
 	import Logo from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+	import Tabs from './shared/Tabs.svelte';
+
+	// tabs
+	let items = ['Current Polls', 'Add New Poll'];
+	let activeItem = 'Current Polls';
+	const tabChange = (e) => {
+		activeItem = e.detail;
+		console.log(activeItem);
+	}
 </script>
 
 <Logo />
 <main>
-
+	<Tabs {items} {activeItem} on:tabChange={tabChange}/>
 </main>
 <Footer />
 
